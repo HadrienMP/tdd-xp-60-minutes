ec4be29c06c659376f86930ffc1fe8111b3f2455
HadrienMP <public@hadrienmp.fr>
2017-09-22 21:40:32 +0200

    Ending in two - Red

--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/NumberGeneratorTest.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/NumberGeneratorTest.java
@@ -14,4 +14,13 @@ class NumberGeneratorTest {
 
         assertThat(number % 2).isEqualTo(0);
     }
+
+    @Test
+    void should_generate_a_number_ending_with_2() {
+        NumberGenerator numberGenerator = new NumberGenerator();
+
+        int number = numberGenerator.generate();
+
+        assertThat(number % 10).isEqualTo(2);
+    }
 }
