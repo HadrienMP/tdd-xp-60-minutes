045b87224b79ccf81f041a27225247bd5d2d70f3
HadrienMP <public@hadrienmp.fr>
2017-10-20 12:53:11 +0200

    POURVU QUE CA MARCHE

---
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/Generator.java
@@ -0,0 +1,10 @@
+package fr.hadrienmp.tdd.xp._60.minutes;
+
+public class Generator {
+
+	public Integer create() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
@@ -1,10 +1,13 @@
 package fr.hadrienmp.tdd.xp._60.minutes;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class MyTest {
     @Test
-    public void myTest() {
-
+    public void generatorShouldReturnOddValue() {
+    	int myNumber = new Generator().create();
+		Boolean isPair = (myNumber % 2 == 0);
+    	Assertions.assertThat(isPair).isTrue();
     }
 }
