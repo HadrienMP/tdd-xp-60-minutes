f948bc03a51ccc9eb4f7c7a890a4a0dae3416438
HadrienMP <public@hadrienmp.fr>
2017-10-24 13:03:22 +0200

    #3 Generation d'un nombre suppérieur à 9

--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/NumberGeneratorTest.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/NumberGeneratorTest.java
@@ -6,8 +6,14 @@ import org.junit.Test;
 
 public class NumberGeneratorTest {
 	@Test
-	public void should_return_two() {
+	public void should_return_even_number() {
 		int number = new NumberGenerator().getNumber();
-		assertThat(number).isEqualTo(2);
+		assertThat(number % 2).isEqualTo(0);
+	}
+	
+	@Test
+	public void should_return_a_number_greater_than_9() throws Exception {
+		int number = new NumberGenerator().getNumber();
+		assertThat(number).isGreaterThan(9);
 	}
 }
