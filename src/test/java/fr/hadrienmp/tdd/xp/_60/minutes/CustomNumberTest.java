0d12f9a1963df09f7739023d229bbc59c6e1adcd
Hadrien Mens-Pellen <public@hadrienmp.fr>
2017-11-10 13:55:30 +0100

    Update CustomNumberTest.java

--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/CustomNumberTest.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/CustomNumberTest.java
@@ -5,39 +5,30 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CustomNumberTest {
+    private int number = CustomNumber.get();
 
     @Test
     void is_even() {
-        int number = CustomNumber.get();
-
         assertThat(number % 2).isEqualTo(0);
     }
 
     @Test
     void ends_by_2() {
-        int number = CustomNumber.get();
-
         assertThat(number % 10).isEqualTo(2);
     }
 
     @Test
     void is_bigger_than_9() {
-        int number = CustomNumber.get();
-
         assertThat(number).isGreaterThan(9);
     }
 
     @Test
     void is_lower_than_100() {
-        int number = CustomNumber.get();
-
         assertThat(number).isLessThan(100);
     }
 
     @Test
     void the_sum_of_its_digits_is_6() {
-        int number = CustomNumber.get();
-
         assertThat(number / 10 + number % 10).isEqualTo(6);
     }
 }
