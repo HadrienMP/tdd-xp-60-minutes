e002b80dc30cb2d0f61b534b97e311cd2df7b5fa
Hadrien Mens-Pellen <hmens@financeactive.com>
2017-11-21 13:16:19 +0100

    Generate number ends by 2

--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/PairNumberGeneratorTest.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/PairNumberGeneratorTest.java
@@ -3,10 +3,19 @@ package fr.hadrienmp.tdd.xp._60.minutes;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MyTest {
+public class PairNumberGeneratorTest {
+
+
     @Test
     public void number_is_pair() {
         PairNumberGenerator pairNumberGenerator = new PairNumberGenerator();
         Assert.assertTrue(pairNumberGenerator.generateNumber()%2==0);
+    }
+
+
+    @Test
+    public void my_number_ends_by_two() {
+        PairNumberGenerator pairNumberGenerator = new PairNumberGenerator();
+        Assert.assertTrue(pairNumberGenerator.generateNumber() % 10 == 2);
     }
 }
