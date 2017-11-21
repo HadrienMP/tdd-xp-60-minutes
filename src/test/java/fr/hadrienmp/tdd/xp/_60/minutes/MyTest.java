24d4172230e27957608a32786498863ce59b25b9
Hadrien Mens-Pellen <hmens@financeactive.com>
2017-11-21 13:01:32 +0100

    Generate number pair

--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
@@ -1,10 +1,12 @@
 package fr.hadrienmp.tdd.xp._60.minutes;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MyTest {
     @Test
-    public void myTest() {
-
+    public void number_is_pair() {
+        PairNumberGenerator pairNumberGenerator = new PairNumberGenerator();
+        Assert.assertTrue(pairNumberGenerator.generateNumber()%2==0);
     }
 }
