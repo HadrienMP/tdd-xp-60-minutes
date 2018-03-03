0a9e704309bec971158cc020f13421a951af3b84
HadrienMP <public@hadrienmp.fr>
2018-03-03 13:20:10 +0200

    Nombre pair

---
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/GenerateurNombre.java
@@ -0,0 +1,7 @@
+package fr.hadrienmp.tdd.xp._60.minutes;
+
+public class GenerateurNombre {
+    public int nombre() {
+        return 0;
+    }
+}
--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
@@ -2,9 +2,13 @@ package fr.hadrienmp.tdd.xp._60.minutes;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MyTest {
     @Test
-    public void myTest() {
-
+    public void le_nombre_est_pair() {
+        GenerateurNombre generateurNombre = new GenerateurNombre();
+        int nombre = generateurNombre.nombre();
+        assertThat(nombre % 2).isEqualTo(0);
     }
 }
