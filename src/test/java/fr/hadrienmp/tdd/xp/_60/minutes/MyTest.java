f8c1c08baac06c8aec2e8fd0204863ed1e07aca4
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-11-07 08:47:31 +0100

    Add an example assertion

--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
@@ -2,9 +2,11 @@ package fr.hadrienmp.tdd.xp._60.minutes;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MyTest {
     @Test
     public void myTest() {
-
+        assertThat(0).isEqualTo(1);
     }
 }
