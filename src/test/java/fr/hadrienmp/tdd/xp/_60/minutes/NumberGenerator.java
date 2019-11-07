dfa6cc6399e4fd136d92be096cd4bc427ef6b989
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-11-07 12:51:06 +0100

    NumberGenerator returns a number ending by 2

--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/NumberGenerator.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/NumberGenerator.java
@@ -2,6 +2,7 @@ package fr.hadrienmp.tdd.xp._60.minutes;
 
 public class NumberGenerator {
     public int generate() {
-        return 0;
+        return 2;
     }
 }
+
--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/NumberGeneratorTest.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/NumberGeneratorTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class MyTest {
+public class NumberGeneratorTest {
 
     final NumberGenerator generator = new NumberGenerator();
 
@@ -12,5 +12,11 @@ public class MyTest {
     public void testNumberIsEven() {
         final int number = generator.generate();
         assertThat(number % 2).isEqualTo(0);
+    }
+
+    @Test
+    public void testNumberEndsBy2() {
+        final int number = generator.generate();
+        assertThat(String.valueOf(number)).endsWith("2");
     }
 }
