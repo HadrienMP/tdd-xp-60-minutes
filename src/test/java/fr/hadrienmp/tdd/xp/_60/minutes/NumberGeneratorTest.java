d8341fc8e5aeb405bec839ac0aba1e177ac070f7
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-11-07 13:04:32 +0100

    NumberGenerator returns a number less than 100

--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/NumberGeneratorTest.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/NumberGeneratorTest.java
@@ -11,19 +11,29 @@ public class NumberGeneratorTest {
     @Test
     public void testNumberIsEven() {
         final int number = generator.generate();
+
         assertThat(number % 2).isEqualTo(0);
     }
 
     @Test
     public void testNumberEndsBy2() {
         final int number = generator.generate();
+
         assertThat(String.valueOf(number)).endsWith("2");
     }
 
     @Test
     public void testNumberIsGreaterThan9() {
         final int number = generator.generate();
+
         assertThat(number).isGreaterThan(9);
+    }
+
+    @Test
+    public void testNumberIsLessThan100(){
+        final int number = generator.generate();
+
+        assertThat(number).isLessThan(100);
     }
 
 }
